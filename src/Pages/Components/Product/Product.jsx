7170e25329ee99c876a89923694b6633c5a815c8
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2023-10-19 15:01:44 +0600

    hero icon added

--- src/Pages/Components/Product/Product.jsx
+++ src/Pages/Components/Product/Product.jsx
@@ -1,12 +1,13 @@
+import { ShoppingCartIcon } from '@heroicons/react/24/solid';
 import React from "react";
 
 const Product = (props) => {
 
-  const {name, img, ratings, seller, price} = props.product
+  const {name, img, ratings, seller, price} = props.product;
   return (
-    <div className="full-cart w-[300px] h-[509px] border mx-auto items-center p-1 rounded-sm relative">
-      <img className="w-[286px] h-[286px] rounded-md mx-auto" src={img} alt="" />
-      <div className="text-container p-3">
+    <div className="full-cart w-[300px] h-[509px] border mx-auto items-center rounded-sm relative">
+      <img className="w-[286px] h-[286px] rounded-md mx-auto m-2" src={img} alt="" />
+      <div className="text-container p-4">
         <h4 className="text-[#0E161A] font-semibold text-[21px]">{name}</h4>
         <h5 className="font-semibold text-[17px] mt-2">Price: ${price}</h5>
         <div className="other-texts mt-4">
@@ -14,7 +15,7 @@ const Product = (props) => {
           <p>Rating : {ratings}</p>
         </div>
       </div>
-      <button className="w-[300px] h-[48px] bg-[#FFE0B3] text-[15px] absolute bottom-0 ">Add to Cart</button>
+      <button className="w-[300px] h-[48px] bg-[#FFE0B3] text-[15px] absolute bottom-0 hover:bg-orange-300">Add to Cart <ShoppingCartIcon className="h-6 w-6 text-black inline-block" /></button>
     </div>
   );
 };
