b21f8db12a98d8d1da18f482b202470d476df578
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2023-10-21 20:21:11 +0600

    save cart to local storage

--- src/Utilities/FakeDb.js
+++ src/Utilities/FakeDb.js
@@ -1,41 +1,39 @@
 
-const addToDb = (id) =>{
-let shoppingCart = {};
+const addToDB = (id) => {
+    let shoppingCart = getShoppingCart();
 
-const storedCart = localStorage.getItem("shopping-cart")
-if(storedCart){
-    shoppingCart = JSON.parse(storedCart)
-}else{
-    shoppingCart = {}
-}
-const quantity = shoppingCart[id]
-if(quantity){
-    const newQuantity = quantity + 1;
-    shoppingCart[id] = newQuantity;
-
-}else{
-    shoppingCart[id] = 1;
-}
-
-localStorage.setItem("shopping-cart", JSON.stringify(shoppingCart))
+    //add quantity;
+    const quantity = shoppingCart[id];
+    if(!quantity){
+        shoppingCart[id] = 1;
+    }else{
+        const newQuantity = quantity + 1;
+        shoppingCart[id] = newQuantity;
+    }
+    localStorage.setItem("shopping-cart", JSON.stringify(shoppingCart));
+};
+const removeFromDB = id => {
+    const shoppingCart = getShoppingCart();
+    if(id in shoppingCart){
+        delete shoppingCart[id];
+        localStorage.setItem('shopping-cart', JSON.stringify(shoppingCart))
+    }
 };
 
-const removeFromDb =id => {
-    const storedCart = localStorage.getItem("Shopping-cart");
+const getShoppingCart = () => {
+    let shoppingCart = {};
+
+    const storedCart = localStorage.getItem('shopping-cart')
     if(storedCart){
-        const shoppingCart = JSON.parse(storedCart);
-        if(id in shoppingCart){
-            delete shoppingCart[id];
-            localStorage.setItem("shopping-cart",JSON.stringify(shoppingCart))
-        }
+        shoppingCart = JSON.parse(storedCart);
     }
-}
+    return shoppingCart
+};
 
 const deleteShoppingCart = () => {
-    localStorage.removeItem("shopping-cart")
-}
+    localStorage.removeItem('shopping-cart');
+};
 
-
-
-export { addToDb, removeFromDb, deleteShoppingCart };
-
+export {
+    addToDB, deleteShoppingCart, getShoppingCart, removeFromDB
+};
