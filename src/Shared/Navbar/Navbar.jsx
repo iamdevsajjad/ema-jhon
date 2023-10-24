1d58e094ed9ae84026ac7f4d34f61681410a575f
Khandaker Sajjad Hossen <sk.sajjad4207@gmail.com>
2023-10-24 21:19:23 +0600

    navbar issue solved

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
     return (
         <header className='bg-[#1C2B35] sticky top-0 z-10'>
-             <span onClick={() => setOpen(!open) } className='md:hidden text-white absolute right-5 top-3'>
+             <span onClick={() => setOpen(!open) } className='md:hidden text-white absolute right-5 top-3 '>
                 {
                     open===true? <XMarkIcon className='w-8'/> :<Bars3Icon className='w-8'/>
                 }
@@ -17,7 +17,7 @@ const Navbar = () => {
                 <div className="logo">
                     <img className='md:w-2/4' src="../../../src/assets/Logo.svg" alt="" />
                 </div>
-                <div className={`nav ${open === true? "block" : "hidden"}`}>
+                <div className={`nav ${open === true? "block" : "hidden md:block"}`}>
                     <ul className='md:flex gap-3 '>
                         <li className='py-3 md:py-0 px-3 md:px-0 text-xl md:text-sm'><Link to="/">Shop</Link></li>
                         <li className='py-3 md:py-0 px-3 md:px-0 text-xl md:text-sm' ><Link to="/orderReview">Order Review</Link></li>
